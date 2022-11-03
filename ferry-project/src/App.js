6ac25db66663ada4b4057e6a6116ec108b2d3612
wszitenhelm <wiktoria.szitenhelm@gmail.com>
2022-11-03 13:55:38 +0000

    add button

--- ferry-project/src/App.js
+++ ferry-project/src/App.js
@@ -6,6 +6,7 @@ import Ideas from './components/Ideas';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutUs from './components/AboutUs';
+import GetAllButton from './components/GetAllButton';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <TextInput/>
       <h2>Enter arrival port:</h2>
       <TextInput/>
+      <GetAllButton/>
       </div>
       <Ideas/>
       <Icons/>
---
+++ ferry-project/src/components/GetAllButton.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+
+const GetAllButton = () => {
+    const [trips, setTrips] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [err, setErr] = useState('');
+
+    const handleClick = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(
+                "http://localhost:5000/api/trips", {
+                    method: "GET", 
+                    headers: {
+                        Accept: 'application/json',
+                    },
+                }
+            ).then((response) => response.json());
+            setTrips(response);
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+        } catch (err) {
+        setErr(err.message);
+        } finally {
+        setIsLoading(false);
+        }
+    };
+
+
+    return (
+        <div className="trips-container">
+            {err && <h2>{err}</h2>}
+            <button onClick={handleClick}>Fetch data</button>
+            {isLoading && <h2>Loading...</h2>}
+            {trips && trips.map((trip) => (
+                <div className="item-container" key={trip._id}>
+                    Id:{trip.ferryId} <div className="title">PortFromId:{trip.portFromId}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+
+
+export default GetAllButton;
