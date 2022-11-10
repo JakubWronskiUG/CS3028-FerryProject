5b83f758122feaf368ca3f010b4894374712292d
GMoar <g.moar.20@abdn.ac.uk>
2022-11-10 14:31:13 +0000

    altered placeholder logo

--- ferry-project/src/components/Header.jsx
+++ ferry-project/src/components/Header.jsx
@@ -5,9 +5,8 @@ const Header = () => {
     return (
         <div className="up-container">   
             <div className="row">
-                <div className="col-lg-9">
-                    <h1>FerryWave</h1>   
-                    <img src="FerrywaveLogo.png" alt="logo" width="300" height="100"></img>
+                <div className="col-lg-9"> 
+                    <h1><img src="FerrywaveLogo.png" alt="logo" width="420" height="110"></img></h1>
                 </div>
                 <div className="col-lg-1">
                     <button type="button">Timetables</button> 
